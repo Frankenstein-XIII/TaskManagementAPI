81390cf53027b3d1c3cbaf5833ed7055b2b152c8
Frankestein-XIII <khagendrad.4510@gmail.com>
2026-04-02 20:12:48 +0800

    feat: error handler in case of server issue

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+
+export const errorHandler = (err:any, req: Request, res: Response, next: NextFunction) =>{
+      console.error(`[Error] ${err.Message}`);
+
+      const status = err.status || 500;
+
+      res.status(status).json({
+            sccess: false,
+            message: err.message || "Internal Server Error!",
+            stack: process.env.NODE_ENV ==='developer'?err.stack :{}
+      });
+};
