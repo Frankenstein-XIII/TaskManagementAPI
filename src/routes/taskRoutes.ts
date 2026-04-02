dff7963a6d62c0175facb7aa50404089bb5d7846
Frankestein-XIII <khagendrad.4510@gmail.com>
2026-04-02 23:06:54 +0800

    feat: add new route for patch to update bug status

--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -1,5 +1,6 @@
 import {Router} from 'express';
 import { TaskController } from '../controllers/TaskController';
+import { taskService } from '../services/TaskService';
 
 
 const  router = Router();
@@ -8,5 +9,7 @@ const  router = Router();
 router.get('/', TaskController.getAllTasks);
 router.post('/bug', TaskController.createBug);
 router.post('/feature', TaskController.createFeature);
+router.delete('/:id', TaskController.deleteTask);
+router.patch('/:id/status', TaskController.updateStatus);
 
 export default router;
