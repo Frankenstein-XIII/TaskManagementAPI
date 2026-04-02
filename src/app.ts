677f11c5165fb551dd2105ffcf5f090452ac6656
Frankestein-XIII <khagendrad.4510@gmail.com>
2026-04-02 20:15:09 +0800

    add new component (loggerMiddleware, erroHandler

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,21 @@
 import express, {Application, Request, Response} from 'express';
+import taskRoutes from './routes/taskRoutes';
+import { loggerMiddleware } from './middleware/loggers';
+import { errorHandler } from './middleware/errorHandler';
+import { error } from 'node:console';
 
 const app: Application  = express();
-const PORT = 300;
+const PORT = 3000;
 
 // middleware to parse JSON
 app.use(express.json());
+app.use(loggerMiddleware);
 
+app.use('/tasks', taskRoutes);
 app.get('/',(req: Request, res: Response) =>{
       res.send("Task Management API is running!");
 });
-
+app.use(errorHandler);
 app.listen(PORT, () =>{
       console.log(`Server is sprinting at http://localhost:${PORT}`);
 });
